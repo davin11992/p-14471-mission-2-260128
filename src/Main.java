60e70e4c1eb39e26e0043f9e9054807986bfa093
Davin <davin11992@gmail.com>
2026-01-28 13:01:06 +0900

    feat: 4ë4단계

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
+        int i=0;
 
-        while (true) {
+        while(true){
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in);
@@ -15,11 +16,14 @@ public class Main {
                 break;
             }
 
-            if (command.equals("등록")) {
+            if(command.equals("등록")){
                 System.out.print("명언 : ");
                 String quote = scanner.next();
                 System.out.print("작가 : ");
                 String writer = scanner.next();
+
+                i++;
+                System.out.println(i+"번 명언이 등록되었습니다.");
             }
         }
     }
