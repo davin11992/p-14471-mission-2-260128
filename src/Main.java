88c4b1a06b89504eb57471b03f0267dd0f7bfa17
Davin <davin11992@gmail.com>
2026-01-28 13:07:21 +0900

    feat: 5단계

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
-        int i=0;
+        int id=0;
+        ArrayList<QuoteSet> quoteSets = new ArrayList<>();
 
+        Scanner scanner = new Scanner(System.in);
         while(true){
             System.out.print("명령) ");
-
-            Scanner scanner = new Scanner(System.in);
             String command = scanner.next();
             scanner.nextLine();
 
@@ -18,12 +19,22 @@ public class Main {
 
             if(command.equals("등록")){
                 System.out.print("명언 : ");
-                String quote = scanner.next();
+                String quote = scanner.nextLine();
                 System.out.print("작가 : ");
-                String writer = scanner.next();
+                String writer = scanner.nextLine();
 
-                i++;
-                System.out.println(i+"번 명언이 등록되었습니다.");
+                id++;
+                quoteSets.add(new QuoteSet(id, quote, writer));
+                System.out.println(id + "번 명언이 등록되었습니다.");
+            }
+
+            if(command.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                for (QuoteSet quoteSet : quoteSets) {
+                    System.out.println(quoteSet.id + " / " + quoteSet.writer + " / " + quoteSet.quote);
+                }
             }
         }
     }
---
+++ src/QuoteSet.java
@@ -0,0 +1,11 @@
+public class QuoteSet {
+    int id;
+    String quote;
+    String writer;
+
+    public QuoteSet(int id, String quote, String writer) {
+        this.id = id;
+        this.quote = quote;
+        this.writer = writer;
+    }
+}
