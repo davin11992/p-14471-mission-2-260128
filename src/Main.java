79a8dafc15a1db3c5fd19cc0922d77efa99d4364
Davin <davin11992@gmail.com>
2026-01-28 17:36:28 +0900

    feat: 8단계

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,18 @@ public class Main {
         ArrayList<QuoteSet> quoteSets = new ArrayList<>();
 
         String deleteRegex = "삭제\\?id=(\\d+)";
-        Pattern pattern = Pattern.compile(deleteRegex);
+        Pattern deletePattern = Pattern.compile(deleteRegex);
+        String updateRegex = "수정\\?id=(\\d+)";
+        Pattern updatePattern = Pattern.compile(updateRegex);
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("명령) ");
-            String command = scanner.next();
-            scanner.nextLine();
+            String command = scanner.nextLine();
 
             if (command.equals("종료")) {
                 break;
-            }
-
-            if (command.equals("등록")) {
+            } else if (command.equals("등록")) {
                 System.out.print("명언 : ");
                 String quote = scanner.nextLine();
                 System.out.print("작가 : ");
@@ -31,9 +30,7 @@ public class Main {
                 id++;
                 quoteSets.add(new QuoteSet(id, quote, writer));
                 System.out.println(id + "번 명언이 등록되었습니다.");
-            }
-
-            if (command.equals("목록")) {
+            } else if (command.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
 
@@ -43,9 +40,9 @@ public class Main {
                 }
             }
 
-            Matcher matcher = pattern.matcher(command);
-            if (matcher.matches()) {
-                int deleteId = Integer.parseInt(matcher.group(1));
+            Matcher delteMatcher = deletePattern.matcher(command);
+            if (delteMatcher.matches()) {
+                int deleteId = Integer.parseInt(delteMatcher.group(1));
                 boolean found = false;
 
                 for (int i = 0; i < quoteSets.size(); i++) {
@@ -59,6 +56,37 @@ public class Main {
 
                 if (!found) {
                     System.out.println(deleteId + "번 명언은 존재하지 않습니다.");
+                }
+            }
+
+            Matcher updateMatcher = updatePattern.matcher(command);
+            if (updateMatcher.matches()) {
+                int updateId = Integer.parseInt(updateMatcher.group(1));
+                boolean found = false;
+
+                for (int i = 0; i < quoteSets.size(); i++) {
+                    if (quoteSets.get(i).id == updateId) {
+                        QuoteSet quoteSet = quoteSets.get(i);
+
+                        System.out.println("명언(기존) : " + quoteSet.quote);
+                        System.out.print("명언 : ");
+                        String updateQuote = scanner.nextLine();
+
+
+                        System.out.println("작가(기존) : " + quoteSet.writer);
+                        System.out.print("작가 : ");
+                        String updateWriter = scanner.nextLine();
+
+                        quoteSet.quote = updateQuote;
+                        quoteSet.writer = updateWriter;
+
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    System.out.println(updateId + "번 명언은 존재하지 않습니다.");
                 }
             }
         }
