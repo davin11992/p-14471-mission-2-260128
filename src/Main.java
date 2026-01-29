df479b7dcb4ad6493eb33e015d37f1c463704783
Davin <davin11992@gmail.com>
2026-01-29 15:12:01 +0900

    refactor: 문자열 상수화

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,59 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+
+    public static final String DELETE_REGEX = "삭제\\?id=(\\d+)";
+    public static final String UPDATE_REGEX = "수정\\?id=(\\d+)";
+
+    public static final String COMMAND_INPUT = "명령) ";
+    public static final String END = "종료";
+    public static final String APP_START = "== 명언 앱 ==";
+    public static final String REGISTRATION = "등록";
+    public static final String QUOTE = "명언 : ";
+    public static final String WRITER = "작가 : ";
+    public static final String LINE = "----------------------";
+    public static final String SLASH = " / ";
+    public static final String LIST = "목록";
+    public static final String NUMBER_WRITER_QUOTE = "번호 / 작가 / 명언";
+    public static final String REGISTER_NOTICE = "번 명언이 등록되었습니다.";
+    public static final String DELETE_NOTICE = "번 명언이 삭제되었습니다.";
+    public static final String NOT_EXIST_NOTICE = "번 명언은 존재하지 않습니다.";
+    public static final String QUOTE_ORIGINAL = "명언(기존) : ";
+    public static final String WRITER_ORIGINAL = "작가(기존) : ";
+
     public static void main(String[] args) {
-        System.out.println("== 명언 앱 ==");
+        System.out.println(APP_START);
         int id = 0;
         ArrayList<QuoteSet> quoteSets = new ArrayList<>();
 
-        String deleteRegex = "삭제\\?id=(\\d+)";
+        String deleteRegex = DELETE_REGEX;
         Pattern deletePattern = Pattern.compile(deleteRegex);
-        String updateRegex = "수정\\?id=(\\d+)";
+        String updateRegex = UPDATE_REGEX;
         Pattern updatePattern = Pattern.compile(updateRegex);
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("명령) ");
+            System.out.print(COMMAND_INPUT);
             String command = scanner.nextLine();
 
-            if (command.equals("종료")) {
+            if (command.equals(END)) {
                 break;
-            } else if (command.equals("등록")) {
-                System.out.print("명언 : ");
+            } else if (command.equals(REGISTRATION)) {
+                System.out.print(QUOTE);
                 String quote = scanner.nextLine();
-                System.out.print("작가 : ");
+                System.out.print(WRITER);
                 String writer = scanner.nextLine();
 
                 id++;
                 quoteSets.add(new QuoteSet(id, quote, writer));
-                System.out.println(id + "번 명언이 등록되었습니다.");
-            } else if (command.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
+                System.out.println(id + REGISTER_NOTICE);
+            } else if (command.equals(LIST)) {
+                System.out.println(NUMBER_WRITER_QUOTE);
+                System.out.println(LINE);
 
                 for (int i = quoteSets.size() - 1; i >= 0; i--) {
                     QuoteSet quoteSet = quoteSets.get(i);
-                    System.out.println(quoteSet.id + " / " + quoteSet.writer + " / " + quoteSet.quote);
+                    System.out.println(quoteSet.id + SLASH + quoteSet.writer + SLASH + quoteSet.quote);
                 }
             }
 
@@ -48,14 +68,14 @@ public class Main {
                 for (int i = 0; i < quoteSets.size(); i++) {
                     if (quoteSets.get(i).id == deleteId) {
                         quoteSets.remove(i);
-                        System.out.println(deleteId + "번 명언이 삭제되었습니다.");
+                        System.out.println(deleteId + DELETE_NOTICE);
                         found = true;
                         break;
                     }
                 }
 
                 if (!found) {
-                    System.out.println(deleteId + "번 명언은 존재하지 않습니다.");
+                    System.out.println(deleteId + NOT_EXIST_NOTICE);
                 }
             }
 
@@ -68,13 +88,13 @@ public class Main {
                     if (quoteSets.get(i).id == updateId) {
                         QuoteSet quoteSet = quoteSets.get(i);
 
-                        System.out.println("명언(기존) : " + quoteSet.quote);
-                        System.out.print("명언 : ");
+                        System.out.println(QUOTE_ORIGINAL + quoteSet.quote);
+                        System.out.print(QUOTE);
                         String updateQuote = scanner.nextLine();
 
 
-                        System.out.println("작가(기존) : " + quoteSet.writer);
-                        System.out.print("작가 : ");
+                        System.out.println(WRITER_ORIGINAL + quoteSet.writer);
+                        System.out.print(WRITER);
                         String updateWriter = scanner.nextLine();
 
                         quoteSet.quote = updateQuote;
@@ -86,7 +106,7 @@ public class Main {
                 }
 
                 if (!found) {
-                    System.out.println(updateId + "번 명언은 존재하지 않습니다.");
+                    System.out.println(updateId + NOT_EXIST_NOTICE);
                 }
             }
         }
